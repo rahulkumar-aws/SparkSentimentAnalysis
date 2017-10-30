ea4956873fec351515c9b3647d8fcea4fce703e0
Rahul Kumar <rahulkumar.aws@gmail.com>
2017-10-31 02:21:44 +0530

    local file added

--- build.sbt
+++ build.sbt
@@ -6,6 +6,11 @@ scalaVersion := "2.11.11"
 
 libraryDependencies += "org.apache.spark" % "spark-core_2.11" % "2.2.0"
 libraryDependencies += "org.apache.spark" % "spark-sql_2.11" % "2.2.0"
+libraryDependencies += "org.apache.spark" % "spark-streaming_2.11" % "2.2.0"
+libraryDependencies += "org.apache.bahir" % "spark-streaming-twitter_2.11" % "2.2.0"
+libraryDependencies += "com.typesafe" % "config" % "1.3.1"
+
+
 
 
 
---
+++ src/main/scala/io/reactiveguru/jobs/twitterSentiment.scala
@@ -0,0 +1,65 @@
+package io.reactiveguru.jobs
+import com.typesafe.config.ConfigFactory
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.twitter.TwitterUtils
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+object twitterSentiment {
+
+  def main(args: Array[String]): Unit = {
+
+
+    if (!Logger.getRootLogger.getAllAppenders.hasMoreElements) {
+      Logger.getRootLogger.setLevel(Level.WARN)
+    }
+
+    val conf = ConfigFactory.load("twitter")
+    val consumerKey = conf.getString("twitter.consumerKey")
+    val consumerSecret = conf.getString("twitter.consumerSecret")
+    val accessToken = conf.getString("twitter.accessToken")
+    val accessTokenSecret = conf.getString("twitter.accessTokenSecret")
+
+    val filters = Array("Netflix","Amazon","Pixel2", "Google", "iPhoneX")
+
+    System.setProperty("twitter4j.oauth.consumerKey", consumerKey)
+    System.setProperty("twitter4j.oauth.consumerSecret", consumerSecret)
+    System.setProperty("twitter4j.oauth.accessToken", accessToken)
+    System.setProperty("twitter4j.oauth.accessTokenSecret", accessTokenSecret)
+
+    val sparkConf = new SparkConf().setAppName("TwitterPopularTags")
+    if (!sparkConf.contains("spark.master")) {
+      sparkConf.setMaster("local[2]")
+    }
+
+    val ssc = new StreamingContext(sparkConf, Seconds(2))
+    val stream = TwitterUtils.createStream(ssc, None, filters)
+
+
+    val hashTags = stream.flatMap(status => status.getText.split(" ").filter(_.startsWith("#")))
+
+    val topCounts60 = hashTags.map((_, 1)).reduceByKeyAndWindow(_ + _, Seconds(60))
+      .map{case (topic, count) => (count, topic)}
+      .transform(_.sortByKey(false))
+
+    val topCounts10 = hashTags.map((_, 1)).reduceByKeyAndWindow(_ + _, Seconds(10))
+      .map{case (topic, count) => (count, topic)}
+      .transform(_.sortByKey(false))
+
+    // Print popular hashtags
+    topCounts60.foreachRDD(rdd => {
+      val topList = rdd.take(10)
+      println("\nPopular topics in last 60 seconds (%s total):".format(rdd.count()))
+      topList.foreach{case (count, tag) => println("%s (%s tweets)".format(tag, count))}
+    })
+
+    topCounts10.foreachRDD(rdd => {
+      val topList = rdd.take(10)
+      println("\nPopular topics in last 10 seconds (%s total):".format(rdd.count()))
+      topList.foreach{case (count, tag) => println("%s (%s tweets)".format(tag, count))}
+    })
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
+
+}
